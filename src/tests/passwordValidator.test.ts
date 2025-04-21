0e6ae8417c7540203c08f6a8ccc01527c733d41d
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-04-21 08:53:56 +0100

    fix: correction in description text on test

--- src/tests/passwordValidator.test.ts
+++ src/tests/passwordValidator.test.ts
@@ -1,7 +1,7 @@
 import { passwordValidator } from '../core/passwordValidator';
 
 describe('PasswordValidator', () => {
-  it('Should return true', () => {
+  it('Should return false when password is an empty text', () => {
     expect(passwordValidator('')).toBe(false);
   });
 });
