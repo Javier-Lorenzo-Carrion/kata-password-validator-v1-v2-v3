ac3be21024be274aeaf75e6043f04927f49cad01
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-04-21 10:19:43 +0100

    refactor: extract minimum characters condition in a constant

--- src/core/passwordValidator.ts
+++ src/core/passwordValidator.ts
@@ -1,5 +1,6 @@
 export function passwordValidator(password: string): boolean {
-  const minimumCharactersAccepted: number = 6;
-  if (password.length >= minimumCharactersAccepted && password.toLowerCase() !== password && password.toUpperCase() !== password && password.includes('_') && password.match(/\d/)) return true;
+  const minimumCharacters: number = 6;
+  const passwordContainsMinimumCharacters: boolean = password.length >= minimumCharacters;
+  if (passwordContainsMinimumCharacters && password.toLowerCase() !== password && password.toUpperCase() !== password && password.includes('_') && password.match(/\d/)) return true;
   return false;
 }
