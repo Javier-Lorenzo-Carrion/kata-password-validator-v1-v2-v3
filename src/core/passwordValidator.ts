49902912ed3b63f197468f6d213bf77074d010dc
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-04-21 10:29:29 +0100

    refactor: extract all the items in the main condition to a explicit constants

--- src/core/passwordValidator.ts
+++ src/core/passwordValidator.ts
@@ -1,6 +1,20 @@
 export function passwordValidator(password: string): boolean {
+
   const minimumCharacters: number = 6;
   const passwordContainsMinimumCharacters: boolean = password.length >= minimumCharacters;
-  if (passwordContainsMinimumCharacters && password.toLowerCase() !== password && password.toUpperCase() !== password && password.includes('_') && password.match(/\d/)) return true;
+  const passwordContainsUpperCaseLetter: boolean = password.toLowerCase() !== password;
+  const passwordContainsLowerCaseLetter: boolean = password.toUpperCase() !== password;
+  const passwordContainsUnderscore = password.includes('_');
+  const passwordContainsDigit: RegExpMatchArray | null = password.match(/\d/);
+
+  if (
+    passwordContainsMinimumCharacters &&
+    passwordContainsUpperCaseLetter &&
+    passwordContainsLowerCaseLetter &&
+    passwordContainsUnderscore &&
+    passwordContainsDigit
+  ) {
+    return true;
+  }
   return false;
 }
