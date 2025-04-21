6321dcfa1d5464b4857b14c51302568151398250
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-04-21 12:45:10 +0100

    refactor: rename test description. feat: implement password validator to reject a password without upper case letter

--- src/core/passwordValidator.ts
+++ src/core/passwordValidator.ts
@@ -1,25 +1,29 @@
-export function passwordValidatorV1(password: string): boolean {
-
+export function passwordValidatorV1(password: string): boolean | undefined {
   const minimumCharacters: number = 6;
   const passwordContainsMinimumCharacters: boolean = password.length >= minimumCharacters;
   const passwordContainsUpperCaseLetter: boolean = password.toLowerCase() !== password;
   const passwordContainsLowerCaseLetter: boolean = password.toUpperCase() !== password;
-  const passwordContainsUnderscore = password.includes('_');
-  const passwordContainsDigit: RegExpMatchArray | null = password.match(/\d/);
+  const passwordContainsUnderscore: boolean = password.includes('_');
+  const passwordContainsDigit: boolean = Boolean(password.match(/\d/));
 
-  if (
+  return (
     passwordContainsMinimumCharacters &&
     passwordContainsUpperCaseLetter &&
     passwordContainsLowerCaseLetter &&
     passwordContainsUnderscore &&
     passwordContainsDigit
-  ) {
-    return true;
-  }
-  return false;
+  );
 }
 
 export function passwordValidatorV2(password: string): boolean {
-  const password1: string [] = password.split('');
-  return password1.length >= 6;
+  const formatedPassword: string[] = [];
+  let countedChars: number = 0;
+
+  password.split('').forEach((char: string) => {
+    countedChars += 1;
+    formatedPassword.push(char.toLowerCase());
+  });
+
+  return countedChars >= 6 && formatedPassword.join('') !== password;
 }
+
--- src/tests/passwordValidator.test.ts
+++ src/tests/passwordValidator.test.ts
@@ -1,34 +1,40 @@
 import { passwordValidatorV1, passwordValidatorV2 } from '../core/passwordValidator';
 
-describe('PasswordValidator version 1', () => {
-  it('Should return true when password has 6 chars or more, has any number, has any underscore, has any uppercase letter and has any lowercase letter', () => {
+describe('PasswordValidator version 1 should', () => {
+  it('accept a valid password', () => {
     expect(passwordValidatorV1('1_Hola')).toBe(true);
   });
-  it('Should return false when password has less than 6 chars, has any number, has any underscore, has any uppercase letter and has any lowercase letter', () => {
+  it('reject a password when has less than 6 characters', () => {
     expect(passwordValidatorV1('1_Hol')).toBe(false);
   });
-  it('Should return false when password has 6 chars or more, has any number, has any underscore, has any lowercase letter but has not any uppercase letter', () => {
+  it('reject a password when has not any uppercase letter', () => {
     expect(passwordValidatorV1('1_hola')).toBe(false);
   });
-  it('Should return false when password has 6 chars or more, has any number, has any underscore, has any uppercase letter but has not any lowercase letter', () => {
+  it('reject a password when has not any lowercase letter', () => {
     expect(passwordValidatorV1('1_HOLA')).toBe(false);
   });
-  it('Should return false when password has 6 chars or more, has any number, has any uppercase letter, has any lowercase letter but has not any underscore, ', () => {
+  it('reject a password when has not any underscore, ', () => {
     expect(passwordValidatorV1('12Hola')).toBe(false);
   });
-  it('Should return false when password has 6 chars or more, has any uppercase letter, has any lowercase letter, has any underscore but has not any number, ', () => {
+  it('reject a password when has not any number, ', () => {
     expect(passwordValidatorV1('g_Hola')).toBe(false);
   });
-  it('Should return false when password is an empty text, ', () => {
+  it('reject a password when password is an empty text, ', () => {
     expect(passwordValidatorV1('')).toBe(false);
   });
 });
 
 describe('PasswordValidator version 2', () => {
-  it('Should return true when password has 6 chars or more, has any number, has any underscore, has any uppercase letter and has any lowercase letter', () => {
+  it('accept a valid password', () => {
     expect(passwordValidatorV2('1_Hola')).toBe(true);
   });
-  it('Should return false when password has less than 6 chars, has any number, has any underscore, has any uppercase letter and has any lowercase letter', () => {
+  it('reject a password when has less than 6 characters', () => {
     expect(passwordValidatorV2('1_Hol')).toBe(false);
   });
+  it('reject a password when has not any uppercase letter', () => {
+    expect(passwordValidatorV2('1_hola')).toBe(false);
+  });
+
 })
+
+
