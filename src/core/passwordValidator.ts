492a166eee7c56c3eed9bdb7cf5ae2fd13dd39c7
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-04-21 10:15:25 +0100

    refactor: delete a magic number and extract into a explicit constant

--- src/core/passwordValidator.ts
+++ src/core/passwordValidator.ts
@@ -1,19 +1,5 @@
-/*
-
-El problema es que si voy paso a paso no me van a pasar los anteriores en verde. Empieza por el caso perfecto simplemente devolviendo true y luego vas viendo cuando debe dar false o piensa algo así no se..
-
-'1_Hola' -> true
-'1_Hol' -> false
-'1_hola' -> false
-'1hol' -> false
-'1' -> false
-'' -> false
-
-Haz que siempre tenga 6 caracteres o mas
-
- */
-
 export function passwordValidator(password: string): boolean {
-  if (password.length >= 6 && password.toLowerCase() !== password && password.toUpperCase() !== password && password.includes('_') && password.match(/\d/)) return true;
+  const minimumCharactersAccepted: number = 6;
+  if (password.length >= minimumCharactersAccepted && password.toLowerCase() !== password && password.toUpperCase() !== password && password.includes('_') && password.match(/\d/)) return true;
   return false;
 }
